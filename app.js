adcdc60bb5b31846a722dc905a61acfc0986b9d7
anadis764 <anadis764@gmail.com>
2022-02-18 11:07:18 +0530

    updated js file

--- app.js
+++ app.js
@@ -1,61 +1,42 @@
-window.addEventListener('load', ()=>{
-    let long;
-    let lat;
-    let temperatureDescription = document.querySelector('.temperature-description');
-    let temperatureDegree = document.querySelector(".temperature-degree");
-    let locationTimezone = document.querySelector('.location-timezone');
-    let temperatureSection = document.querySelector('.temperature');
-    const temperatureSpan = document.querySelector('.temperature span')
+window.addEventListener("load", () => {
+	let long;
+	let lat;
+	let temperatureDescription = document.querySelector(".temperature-description");
+	let temperatureDegree = document.querySelector(".temperature-degree");
+	let locationTimezone = document.querySelector(".location-timezone");
+	let imgIcon = document.getElementById("icon");
+	let temperatureSection = document.querySelector(".temperature");
+	let temperatureSpan = document.querySelector(".temperature span");
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position =>{
-            long  = position.coords.longitude;
-            lat = position.coords.latitude;
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(async (position) => {
+			long = position.coords.longitude;
+			lat = position.coords.latitude;
 
+			// Fetch weather
+			const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=e822515d9d70fb424286ce57415f7b3c`;
+			const response = await fetch(api);
+			const data = await response.json();
+            console.log(data);
+			// Set elements
+			temperatureDegree.textContent = data.main.temp;
+			temperatureDescription.textContent = data.weather[0].description.toUpperCase();
+			locationTimezone.textContent = data.name;
+			imgIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=e822515d9d70fb424286ce57415f7b3c
-            `
+			// Formula for conversion
+			let fahrenheit = data.main.temp * (9 / 5) + 32;
 
-            fetch(api)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-                const {temp} = data.main;
-                const {0: {description}} = data.weather;
-                const {name} = data;
-                const icon =  data.weather[0].icon;
-                //Set DOM Elements from the API
-                
-                temperatureDescription.textContent = description.toUpperCase();;
-                locationTimezone.textContent = name;
-                document.getElementById('image-box').src ='http://openweathermap.org/img/wn/' + icon + '@2x.png';
-
-               
-                // Change Temperature to Celsius/Farenheit
-                let celsius = (temp - 273.15);
-                let Farenheit = ((temp - 273.15) * (9/5) + 32) ;
-                    temperatureSection.addEventListener('click', () => {
-                        if(temperatureSpan.textContent === "C"){
-                            temperatureSpan.textContent = "F";
-                            temperatureDegree.textContent = Math.floor(Farenheit);
-                        }
-                        else{
-                            temperatureSpan.textContent = "C";
-                            temperatureDegree.textContent = Math.floor(celsius);
-                        }
-                    })
-
-
-            })
-        });
-
-       
-    }
-    // const icon = wInfo.weather[0].icon; // For instance "09d"
-    // <Image source={{ uri: ``http://openweathermap.org/img/w/${icon}.png`` }} />
-
-   
+			// Change temp units
+			temperatureSection.addEventListener("click", () => {
+				if (temperatureSpan.textContent === "C") {
+					temperatureSpan.textContent = "F";
+					temperatureDegree.textContent = Math.floor(fahrenheit);
+				} else {
+					temperatureSpan.textContent = "C";
+					temperatureDegree.textContent = Math.floor(data.main.temp);
+				}
+			});
+		});
+	}
 });
